de6f1eee6d4836e0784611a2e42a28ba292095c2
Takuya Aso <milanista224@gmail.com>
2021-11-22 22:08:25 +0900

    ボタン実装

--- VoiceOperationSample/ContentView.swift
+++ VoiceOperationSample/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button {
+                } label: {
+                    Text("SFSpeechRecognizer")
+                        .padding()
+                }
+            }
+            .navigationTitle("音声操作サンプル？")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
