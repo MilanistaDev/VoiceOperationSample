608f327c4c86e3d99adfd4d1d5b845c2ebb68d29
Takuya Aso <milanista224@gmail.com>
2021-11-22 22:03:35 +0900

    Initial Commit

---
+++ VoiceOperationSample/VoiceOperationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOperationSampleApp.swift
+//  VoiceOperationSample
+//
+//  Created by Takuya Aso on 2021/11/22.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOperationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
