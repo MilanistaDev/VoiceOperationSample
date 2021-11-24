f40a11db16b7522da22ea0be07c3d4ccc94f4df9
Takuya Aso <milanista224@gmail.com>
2021-11-24 23:17:55 +0900

    音声認識のための音声セッションのセットアップ

--- VoiceOperationSample/VoiceOperationViewModel.swift
+++ VoiceOperationSample/VoiceOperationViewModel.swift
@@ -17,12 +17,13 @@ final class VoiceOperationViewModel: ObservableObject {
 
     private var audioEngine: AVAudioEngine
     private var speechRecognizer: SFSpeechRecognizer
-    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest
     private var recognitionTask: SFSpeechRecognitionTask?
 
     init() {
         self.audioEngine = AVAudioEngine()
         self.speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
     }
 
     /// 音声認識の許可状態をチェック
@@ -32,6 +33,7 @@ final class VoiceOperationViewModel: ObservableObject {
                 switch authStatus {
                 case .authorized:
                     self.isAuthorized = true
+                    self.setupAudioSession()
                 case .denied, .restricted, .notDetermined:
                     self.isAuthorized = false
                     self.isShowAlert = true
@@ -42,12 +44,37 @@ final class VoiceOperationViewModel: ObservableObject {
         }
     }
 
+    private func setupAudioSession() {
+        do {
+            // オンデバイス音声認識対応しているか
+            if speechRecognizer.supportsOnDeviceRecognition {
+                recognitionRequest.requiresOnDeviceRecognition = true
+            }
+            // 中間結果の取得を行うか
+            recognitionRequest.shouldReportPartialResults = true
+            // オーディオセッションの準備
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
     func toggleRecognitionStatus() {
         isRecognized.toggle()
         if isRecognized {
-
+            startRecognition()
         } else {
-
+            startRecognition()
         }
+    }
+
+    private func startRecognition() {
+
+    }
+
+    private func stopRecognition() {
+
     }
 }
